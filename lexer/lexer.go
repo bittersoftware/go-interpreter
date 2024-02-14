e5d99de8e2d4e2e49e27ac02a0b993b4cf78b1b0
Thiago <bittersoftware@gmail.com>
2024-02-14 20:11:26 +0100

    add support to double char operators

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -5,7 +5,7 @@ import "monkey/token"
 type Lexer struct {
 	input        string
 	position     int  // curr pos in input (points to curr char)
-	readPosition int  // current readin ps in input (after curr char)
+	readPosition int  // next char after position
 	ch           byte // curr char unde examination
 }
 
@@ -25,6 +25,15 @@ func (l *Lexer) readChar() {
 	}
 	l.position = l.readPosition
 	l.readPosition += 1
+}
+
+// Peek ahead to next char. No other action. Positions remain the same
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
 }
 
 // keep mapping string until find a non-letter char
@@ -59,13 +68,25 @@ func (l *Lexer) NextToken() token.Token {
 
 	switch l.ch {
 	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			tok = token.Token{Type: token.EQ, Literal: string(ch) + string(l.ch)}
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
 	case '+':
 		tok = newToken(token.PLUS, l.ch)
 	case '-':
 		tok = newToken(token.MINUS, l.ch)
 	case '!':
-		tok = newToken(token.BANG, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			tok = token.Token{Type: token.NOT_EQ, Literal: string(ch) + string(l.ch)}
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
 	case '/':
 		tok = newToken(token.SLASH, l.ch)
 	case '*':
